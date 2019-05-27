77dbef3575e9952a97856c26cbfaffe539e9ab36
Jakub Igla <jakub.igla@gmail.com>
2019-05-27 22:08:27 +0100

    Add frontend and backend subnets

--- manifests/network.tf
+++ manifests/network.tf
@@ -1,13 +1,12 @@
 variable "vnet_cidr" {
-  default = ["10.0.0.0/16"]
-  type = "list"
+  default = "10.0.0.0/16"
 }
 
 resource "azurerm_virtual_network" "main" {
   resource_group_name = azurerm_resource_group.network.name
   location            = var.location
   name                = "${local.resource_affix}main-vnet"
-  address_space       = var.vnet_cidr
+  address_space       = [var.vnet_cidr]
 
   tags = "${merge(
     local.common_tags,
@@ -15,4 +14,18 @@ resource "azurerm_virtual_network" "main" {
       "Role", "MainVnet"
     )
   )}"
+}
+
+resource "azurerm_subnet" "frontend" {
+  address_prefix = cidrsubnet(var.vnet_cidr, 8, 0)
+  name = "frontend"
+  resource_group_name = azurerm_resource_group.network.name
+  virtual_network_name = azurerm_virtual_network.main.name
+}
+
+resource "azurerm_subnet" "backend" {
+  address_prefix = cidrsubnet(var.vnet_cidr, 8, 1)
+  name = "backend"
+  resource_group_name = azurerm_resource_group.network.name
+  virtual_network_name = azurerm_virtual_network.main.name
 }
